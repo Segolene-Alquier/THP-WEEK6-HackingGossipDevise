107eab8c91a2841964a946b04a08b3c2208d679f
Segolene Alquier <segolene.alquier@yahoo.com>
2018-02-13 23:25:12 +0100

    code promo ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :username, presence: true
   has_many :gossips
+
+  before_validation :codepromo
+
+  def codepromo
+    unless ["petitcurieux"].include? promocode
+    errors.add :email, "Ton code promo n'est pas valide, bien tenté !"
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213133647) do
+ActiveRecord::Schema.define(version: 20180213220058) do
 
   create_table "gossips", force: :cascade do |t|
     t.text "content"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20180213133647) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "promocode"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
